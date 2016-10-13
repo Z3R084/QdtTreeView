02bdb23669e9eae2215c3939f07df2b6d6a627a3
Christian Quast <christian.quast84@googlemail.com>
2016-10-13 16:11:51 +0200

    added models

---
+++ src/app/tree-view/tree-view.model.ts
@@ -0,0 +1,8 @@
+﻿import { Injectable } from '@angular/core';
+import { TreeViewItem } from '../tree-view-item/tree-view-item.model';
+
+@Injectable()
+export class TreeViewModel {
+    roots: TreeViewItem[];
+
+}
