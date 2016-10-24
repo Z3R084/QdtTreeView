8331eb4798066b78b41f801750737771d70f46a0
Christian Quast <christian.quast84@googlemail.com>
2016-10-24 15:46:34 +0200

    updated drag and drop handling

--- src/app/tree-view/tree-view.component.ts
+++ src/app/tree-view/tree-view.component.ts
@@ -1,5 +1,6 @@
 ﻿import { Component, OnChanges, Input, ViewEncapsulation } from '@angular/core';
 import { TreeViewModel } from './tree-view.model';
+import { DragulaService } from 'ng2-dragula/ng2-dragula';
 
 @Component({
     selector: 'tree-view',
@@ -10,12 +11,28 @@ import { TreeViewModel } from './tree-view.model';
 })
 
 export class TreeViewComponent implements OnChanges {
-    treeViewItems: Object[];
-    constructor(public treeModel: TreeViewModel) { }
+    constructor(public treeModel: TreeViewModel, private dragulaService: DragulaService) {
+        dragulaService.setOptions('tree-drag', {
+            accepts: (el: Element, target: Element, source: Element, sibling: Element): boolean => {
+                return !el.contains(target);
+            },
+            moves: (el: Element, container: Element, handle: Element): boolean => {
+                return el.classList.contains('entry');
+            }
+        });
+        dragulaService.dropModel.subscribe((value) => {
+            this.onDropModel(value.slice( 1 ));
+        });
+    }
 
     @Input() set treeItems(treeItems: any[]) { }
 
     ngOnChanges(changes) {
         this.treeModel.setData({ treeItems: changes.treeItems && changes.treeItems.currentValue });
+    }
+
+    onDropModel(args: any): void {
+        let [el, target, source] = args;
+        console.log(this.treeModel.roots);
     }
 }
