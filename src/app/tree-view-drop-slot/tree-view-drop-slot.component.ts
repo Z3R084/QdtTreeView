4255ce5f930f1ce3c2ebbe3a297bcb068b83dd50
Christian Quast <christian.quast84@googlemail.com>
2016-10-28 14:01:02 +0200

    updated drag n drop

---
+++ src/app/tree-view-drop-slot/tree-view-drop-slot.component.ts
@@ -0,0 +1,47 @@
+﻿import { Component, Input, Output, trigger, state, style, transition, animate, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'tree-view-drop-slot',
+    templateUrl: './tree-view-drop-slot.component.html',
+    styleUrls: ['./tree-view-drop-slot.component.less']
+    //animations: [
+    //    trigger('viewState', [
+    //        state('true', style({
+    //            height: '*',
+    //            opacity: 1
+    //        })),
+    //        state('false', style({
+    //            height: '0px',
+    //            opacity: 0
+    //        })),
+    //        transition('1 => 0', animate('300ms')),
+    //        transition('0 => 1', animate('300ms'))
+    //    ])
+    //]
+})
+
+export class TreeViewSectionComponent {
+    @Input() treeViewItems: Object[];
+    hoverElement: Object;
+    @Input() selectedItem: Object;
+    @Output() onSelected = new EventEmitter<Object>();
+
+    overElement($event: MouseEvent, item: Object): void {
+        $event.stopPropagation();
+        //$event.preventDefault();
+        this.hoverElement = item;
+    }
+
+    selectItem($event: MouseEvent, item: Object): void {
+        $event.stopPropagation();
+        $event.preventDefault();
+        this.selectedItem = item;
+        this.onSelected.emit(this.selectedItem);
+    }
+
+    selectedItemChanged(item: Object) {
+        if (this.selectedItem !== item) {
+            this.selectedItem = null;
+        }
+    }
+}
