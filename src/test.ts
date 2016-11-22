0111605355fb3ebe6d15bdf75d104696485e38c2
Christian Quast <christian.quast84@googlemail.com>
2016-11-22 15:40:43 +0100

    updated packages

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-import './polyfills.ts';
+﻿import './polyfills.ts';
 
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy.js';
@@ -6,6 +6,8 @@ import 'zone.js/dist/sync-test';
 import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
+import { getTestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare var __karma__: any;
@@ -15,20 +17,24 @@ declare var require: any;
 __karma__.loaded = function () {};
 
 
-Promise.all([
-  System.import('@angular/core/testing'),
-  System.import('@angular/platform-browser-dynamic/testing')
-])
-  // First, initialize the Angular testing environment.
-  .then(([testing, testingBrowser]) => {
-    testing.getTestBed().initTestEnvironment(
-      testingBrowser.BrowserDynamicTestingModule,
-      testingBrowser.platformBrowserDynamicTesting()
-    );
-  })
+//Promise.all([
+//  System.import('@angular/core/testing'),
+//  System.import('@angular/platform-browser-dynamic/testing')
+//])
+// First, initialize the Angular testing environment.
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+  //.then(([testing, testingBrowser]) => {
+  //  testing.getTestBed().initTestEnvironment(
+  //    testingBrowser.BrowserDynamicTestingModule,
+  //    testingBrowser.platformBrowserDynamicTesting()
+  //  );
+  //})
   // Then we find all the tests.
-  .then(() => require.context('./', true, /\.spec\.ts/))
+let context = require.context('./', true, /\.spec\.ts/);
+  //.then(() => require.context('./', true, /\.spec\.ts/))
   // And load the modules.
-  .then(context => context.keys().map(context))
+context.keys().map(context);
+  //.then(context => context.keys().map(context))
   // Finally, start Karma to run the tests.
-  .then(__karma__.start, __karma__.error);
+__karma__.start();
+  //.then(__karma__.start, __karma__.error);
